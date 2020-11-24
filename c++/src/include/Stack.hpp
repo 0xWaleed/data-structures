cb7328b36011a47635b125349b3556a10e0f73ed
BISOON <20063083+BISOON@users.noreply.github.com>
2020-11-24 06:10:59 +0300

    cpp - stack implementation #4

---
+++ c++/src/include/Stack.hpp
@@ -0,0 +1,123 @@
+
+
+#ifndef STACK_HPP
+#define STACK_HPP
+
+
+#include <cstdint>
+#include <any>
+#include <array>
+
+
+template<typename T = int>
+class Stack
+{
+public:
+
+    static const int DEFAULT_MAX_SIZE = 64;
+
+    Stack();
+
+    explicit Stack(uint32_t maxSize);
+
+    [[nodiscard]] bool isEmpty() const;
+
+    [[nodiscard]] bool isFull() const;
+
+    [[nodiscard]] size_t size() const;
+
+    [[nodiscard]] size_t maxSize() const;
+
+    void push(const T& item);
+
+    [[nodiscard]] T peek();
+
+    T pop();
+
+private:
+    size_t m_maxSize;
+
+    size_t m_size;
+
+    bool m_isEmpty;
+    bool m_isFull;
+
+    T m_items[DEFAULT_MAX_SIZE];
+};
+
+template<typename T>
+Stack<T>::Stack(): Stack::Stack(DEFAULT_MAX_SIZE)
+{
+
+}
+
+template<typename T>
+Stack<T>::Stack(uint32_t maxSize):
+        m_maxSize(maxSize),
+        m_size(0),
+        m_isEmpty(true),
+        m_isFull(false)
+{
+
+}
+
+template<typename T>
+bool Stack<T>::isEmpty() const
+{
+    return this->m_isEmpty;
+}
+
+template<typename T>
+bool Stack<T>::isFull() const
+{
+    return this->m_isFull;
+}
+
+template<typename T>
+size_t Stack<T>::size() const
+{
+    return this->m_size;
+}
+
+template<typename T>
+size_t Stack<T>::maxSize() const
+{
+    return this->m_maxSize;
+}
+
+template<typename T>
+void Stack<T>::push(const T& item)
+{
+    if (this->size() >= this->maxSize())
+    {
+        return;
+    }
+    this->m_isEmpty = false;
+    this->m_items[this->m_size++] = item;
+    this->m_isFull = this->size() == this->maxSize();
+}
+
+template<typename T>
+T Stack<T>::peek()
+{
+    if (size() == 0)
+    {
+        return T();
+    }
+    return this->m_items[this->size() - 1];
+}
+
+template<typename T>
+T Stack<T>::pop()
+{
+    if (this->size() == 0)
+    {
+        return T();
+    }
+    T v = this->m_items[this->m_size-- - 1];
+    this->m_isEmpty = this->size() == 0;
+    this->m_isFull = this->size() == this->maxSize();
+    return v;
+}
+
+#endif //STACK_HPP
--- c++/tests/Stack.test.cpp
+++ c++/tests/Stack.test.cpp
@@ -1,2 +1,163 @@
+#include <catch2/catch.hpp>
+#include <Stack.hpp>
 
+TEST_CASE("Stack")
+{
+    SECTION("initialization")
+    {
+        SECTION("should be empty")
+        {
+            Stack s;
+            REQUIRE(s.isEmpty() == true);
+        }
+
+        SECTION("should not be full")
+        {
+            Stack s;
+            REQUIRE(s.isFull() == false);
+        }
+
+        SECTION("size should be zero")
+        {
+            Stack s;
+            REQUIRE(s.size() == 0);
+        }
+
+        SECTION("default max size should be 64")
+        {
+            Stack s;
+            REQUIRE(s.maxSize() == 64);
+        }
+
+        SECTION("max size can be changed")
+        {
+            Stack s(2);
+            REQUIRE(s.maxSize() == 2);
+        }
+    }
+
+    SECTION("push")
+    {
+        SECTION("should increment the size")
+        {
+            Stack s;
+            s.push(5);
+            REQUIRE(s.size() == 1);
+        }
+
+        SECTION("should push the exact item")
+        {
+            Stack s;
+            s.push(5);
+            REQUIRE(s.peek() == 5);
+        }
+
+        SECTION("should push multiple items")
+        {
+            Stack s;
+            s.push(5);
+            s.push(55);
+            s.push(555);
+            REQUIRE(s.peek() == 555);
+        }
+
+        SECTION("should not push when stack is full")
+        {
+            Stack s(2);
+            s.push(5);
+            s.push(55);
+            s.push(555);
+            REQUIRE(s.peek() == 55);
+            REQUIRE(s.size() == 2);
+        }
+    }
+
+    SECTION("pop")
+    {
+        SECTION("decrements the size")
+        {
+            Stack s;
+            s.push(5);
+            s.push(55);
+            s.push(555);
+            s.pop();
+            REQUIRE(s.size() == 2);
+        }
+
+        SECTION("should not decrement the size of empty stack and return default value")
+        {
+            Stack s;
+            int value = s.pop();
+            REQUIRE(s.size() == 0);
+            REQUIRE(value == 0);
+        }
+
+        SECTION("return last pushed item")
+        {
+            Stack s;
+            s.push(5);
+            s.push(55);
+            s.push(555);
+            REQUIRE(s.pop() == 555);
+        }
+    }
+
+    SECTION("peek")
+    {
+        SECTION("return last pushed item without removing it")
+        {
+            Stack s;
+            s.push(5);
+            s.push(55);
+            s.push(555);
+            REQUIRE(s.peek() == 555);
+            REQUIRE(s.size() == 3);
+            REQUIRE(s.pop() == 555);
+        }
+
+        SECTION("return default value when stack is empty")
+        {
+            Stack<int> s;
+            REQUIRE(s.peek() == int());
+        }
+    }
+
+    SECTION("is empty")
+    {
+        SECTION("should not be empty when stack has item(s)")
+        {
+            Stack s;
+            s.push(5);
+            REQUIRE(s.isEmpty() == false);
+        }
+
+        SECTION("should be empty when removing all items from stack")
+        {
+            Stack s;
+            s.push(5);
+            s.pop();
+            REQUIRE(s.isEmpty() == true);
+        }
+    }
+
+    SECTION("is full")
+    {
+        SECTION("stack should be full when size == max size")
+        {
+            Stack s(2);
+            s.push(5);
+            s.push(500);
+            REQUIRE(s.isFull() == true);
+        }
+
+        SECTION("stack should not be empty when removing item")
+        {
+            Stack s(2);
+            s.push(5);
+            s.push(500);
+            s.pop();
+            REQUIRE(s.isFull() == false);
+        }
+    }
+}
 
