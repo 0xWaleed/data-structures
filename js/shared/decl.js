ee7efb5f4a25c34893c0ece26d6b37f316766276
BISOON <20063083+BISOON@users.noreply.github.com>
2020-12-03 15:34:23 +0300

    js - all common declarations in one place

---
+++ js/shared/decl.js
@@ -0,0 +1,6 @@
+const expect = chai.expect;
+const describe = Mocha.describe;
+const it = Mocha.it;
+const xit = Mocha.xit;
+const afterEach = Mocha.afterEach;
+const beforeEach = Mocha.beforeEach;
--- js/stack/stack.test.js
+++ js/stack/stack.test.js
@@ -1,10 +1,3 @@
-const expect = chai.expect;
-const describe = Mocha.describe;
-const it = Mocha.it;
-const xit = Mocha.xit;
-const afterEach = Mocha.afterEach;
-const beforeEach = Mocha.beforeEach;
-
 
 describe('Stack', () =>
 {
